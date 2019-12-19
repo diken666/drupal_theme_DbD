063a5c0480ab096ada63e5e67cf735daba06d853
diken666 <diken666@163.com>
2019-12-19 16:03:19 +0800

    12/19

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-console.log(123);
-let a = document.getElementById('mymenu');
-console.log(a);
-console.log()
+let menuItems = document.getElementsByClassName('my-menu')[0].children;
+for (let i=0; i<menuItems.length; i++) {
+  menuItems[i].addEventListener('mouseenter', ()=>{
+    console.log("->", i)
+    for (let item of menuItems[i].children) {
+      if (item.tagName === 'UL') {
+        console.log("-->", i)
+        item.style.display = 'block';
+      }
+    }
+  });
+  menuItems[i].addEventListener('mouseleave', ()=>{
+    for (let item of menuItems[i].children) {
+      if (item.tagName === 'UL') {
+        item.style.display = 'none';
+      }
+    }
+  })
+}
